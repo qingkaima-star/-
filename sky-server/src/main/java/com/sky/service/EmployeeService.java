520c320f2a5e041de1b2167b9f644cdd29765a0a
one <1203513645@qq.com>
2026-05-31 23:04:15 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -20,4 +20,8 @@ public interface EmployeeService {
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void satrtOrStop(Integer status, long id);
+
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
